8a6da63747764f4df8d887135c771c573cb1487f
Aditya Verma <aditya.verma@flipkart.com>
2021-03-13 21:36:59 +0530

    fix typos

--- src/bot.js
+++ src/bot.js
@@ -25,11 +25,11 @@ client.on('message', async (message) => {
           'By design, the entire Mina blockchain is and will always be about 22kb - the size of a couple of tweets. So anyone with a smartphone will be able to sync and verify the network in seconds.')
     }
     else if (CMD_NAME.toLowerCase() === 'sale') {
-      message.channel.send('There is no annoucement of sale at the momement, keep an eye on announcement channel.\n' +
-          'Meanwhile you can read the tokenomic here: https://minaprotocol.com/blog/mina-token-distribution-and-supply ')
+      message.channel.send('There is no annoucement of sale at the moment, keep an eye on announcement channel.\n' +
+          'Meanwhile you can read the tokenomics here: https://minaprotocol.com/blog/mina-token-distribution-and-supply ')
     }
     else if (CMD_NAME.toLowerCase() === 'earn') {
-      message.channel.send('There is an ongoing community channel. Top 25 will earn 20k Mina Tokens, To take part refer:\n' +
+      message.channel.send('There is an ongoing community challenge. Top 25 will earn 20k Mina Tokens each, To take part refer:\n' +
           'https://forums.minaprotocol.com/t/mina-community-leaderboard-challenges/1449')
     }
     else if (CMD_NAME.toLowerCase() === 'learn') {
