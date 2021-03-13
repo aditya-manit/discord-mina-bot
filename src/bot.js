e15bfbfe5fda805efd8f9cc2fe6104c3b792fe0c
Aditya Verma <aditya.verma@flipkart.com>
2021-03-13 17:03:33 +0530

    add commands

--- src/bot.js
+++ src/bot.js
@@ -1,17 +1,13 @@
 require("dotenv").config();
 
-const { Client, WebhookClient } = require('discord.js');
+const { Client } = require('discord.js');
 
-const client = new Client({
-  partials: ['MESSAGE', 'REACTION']
-});
 
-const webhookClient = new WebhookClient(
-  process.env.WEBHOOK_ID,
-  process.env.WEBHOOK_TOKEN,
-);
 
-const PREFIX = "$";
+
+const client = new Client();
+
+const PREFIX = "!";
 
 client.on('ready', () => {
   console.log(`${client.user.tag} has logged in.`);
@@ -24,80 +20,34 @@ client.on('message', async (message) => {
       .trim()
       .substring(PREFIX.length)
       .split(/\s+/);
-    if (CMD_NAME === 'kick') {
-      if (!message.member.hasPermission('KICK_MEMBERS'))
-        return message.reply('You do not have permissions to use that command');
-      if (args.length === 0)
-        return message.reply('Please provide an ID');
-      const member = message.guild.members.cache.get(args[0]);
-      if (member) {
-        member
-          .kick()
-          .then((member) => message.channel.send(`${member} was kicked.`))
-          .catch((err) => message.channel.send('I cannot kick that user :('));
-      } else {
-        message.channel.send('That member was not found');
-      }
-    } else if (CMD_NAME === 'ban') {
-      if (!message.member.hasPermission('BAN_MEMBERS'))
-        return message.reply("You do not have permissions to use that command");
-      if (args.length === 0) return message.reply("Please provide an ID");
-      try {
-        const user = await message.guild.members.ban(args[0]);
-        message.channel.send('User was banned successfully');
-      } catch (err) {
-        console.log(err);
-        message.channel.send('An error occured. Either I do not have permissions or the user was not found');
-      }
-    } else if (CMD_NAME === 'announce') {
-      console.log(args);
-      const msg = args.join(' ');
-      console.log(msg);
-      webhookClient.send(msg);
+    if (CMD_NAME.toLowerCase() === 'mina') {
+      message.channel.send('**Mina is the world\'s lightest blockchain, powered by participants.**\n' +
+          'By design, the entire Mina blockchain is and will always be about 22kb - the size of a couple of tweets. So anyone with a smartphone will be able to sync and verify the network in seconds.')
+    }
+    else if (CMD_NAME.toLowerCase() === 'sale') {
+      message.channel.send('There is no annoucement of sale at the momement, keep an eye on announcement channel.\n' +
+          'Meanwhile you can read the tokenomic here: https://minaprotocol.com/blog/mina-token-distribution-and-supply ')
+    }
+    else if (CMD_NAME.toLowerCase() === 'earn') {
+      message.channel.send('There is an ongoing community channel. Top 25 will earn 20k Mina Tokens, To take part refer:\n' +
+          'https://forums.minaprotocol.com/t/mina-community-leaderboard-challenges/1449')
+    }
+    else if (CMD_NAME.toLowerCase() === 'learn') {
+      message.channel.send('This is a great source to start with:\n' +
+          'https://minaprotocol.com/get-started#knowledge-base')
+    }
+    else if (CMD_NAME.toLowerCase() === 'help') {
+      message.channel.send('Possible Commands: \n' +
+          '```!mina\n' +
+          '!sale\n' +
+          '!earn\n' +
+          '!learn\n```')
+    }
+    else {
+      message.channel.send('Sorry ! But I don\'t know, what to say !')
     }
   }
 });
 
-client.on('messageReactionAdd', (reaction, user) => {
-  const { name } = reaction.emoji;
-  const member = reaction.message.guild.members.cache.get(user.id);
-  if (reaction.message.id === '738666523408990258') {
-    switch (name) {
-      case '🍎':
-        member.roles.add('738664659103776818');
-        break;
-      case '🍌':
-        member.roles.add('738664632838782998');
-        break;
-      case '🍇':
-        member.roles.add('738664618511171634');
-        break;
-      case '🍑':
-        member.roles.add('738664590178779167');
-        break;
-    }
-  }
-});
-
-client.on('messageReactionRemove', (reaction, user) => {
-  const { name } = reaction.emoji;
-  const member = reaction.message.guild.members.cache.get(user.id);
-  if (reaction.message.id === '738666523408990258') {
-    switch (name) {
-      case '🍎':
-        member.roles.remove('738664659103776818');
-        break;
-      case '🍌':
-        member.roles.remove('738664632838782998');
-        break;
-      case '🍇':
-        member.roles.remove('738664618511171634');
-        break;
-      case '🍑':
-        member.roles.remove('738664590178779167');
-        break;
-    }
-  }
-});
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
